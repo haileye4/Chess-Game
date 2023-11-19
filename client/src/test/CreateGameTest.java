381792c52ad251896228a48b2773185e6261ca04
haileye4 <hailey.e.johnson3@gmail.com>
2023-11-18 18:48:02 -0700

    createGame first test

--- client/src/test/CreateGameTest.java
+++ client/src/test/CreateGameTest.java
@@ -1,10 +1,41 @@
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import models.AuthToken;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import request.CreateGameRequest;
+import responses.CreateGameResponse;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
 
 public class CreateGameTest {
     ServerFacade server = new ServerFacade();
+    GameDAO games = new GameDAO();
+    AuthDAO tokens = new AuthDAO();
 
     @Test
-    void createGame() {
+    void createGame() throws SQLException, DataAccessException, IOException, URISyntaxException {
+        games.clear();
+        tokens.clear();
+
+        tokens.create(new AuthToken("specialToken", "user"));
+
+        CreateGameRequest request = new CreateGameRequest("gameName");
+
+        CreateGameResponse response = server.CreateGame(request, "specialToken");
+
+        Assertions.assertEquals("gameName", games.read().get(0).getGameName(), "Game was not added to database");
+
+        games.clear();
+        tokens.clear();
+
+    }
+
+    @Test
+    void badCreateGame() {
 
     }
 }
